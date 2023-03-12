ae97ee85279f4ce7136da9d90ea70feb0af2299b
magdaPulter <magda.pulter@gmail.com>
2023-03-12 19:17:55 +0100

    Store products page added

--- src/app/components/store-products/store-products.component-module.ts
+++ src/app/components/store-products/store-products.component-module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 import { StoreProductsComponent } from './store-products.component';
 
 @NgModule({
-  imports: [],
+  imports: [RouterModule, CommonModule, ReactiveFormsModule],
   declarations: [StoreProductsComponent],
   providers: [],
   exports: [StoreProductsComponent]
--- src/app/components/store-products/store-products.component.ts
+++ src/app/components/store-products/store-products.component.ts
@@ -1,4 +1,14 @@
 import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Observable, combineLatest } from 'rxjs';
+import { filter, map, startWith, switchMap } from 'rxjs/operators';
+import { CategoryModel } from '../../models/category.model';
+import { StoreModel } from '../../models/store.model';
+import { ProductModel } from '../../models/product.model';
+import { CategoryService } from '../../services/category.service';
+import { StoreService } from '../../services/store.service';
+import { ProductService } from '../../services/product.service';
 
 @Component({
   selector: 'app-store-products',
@@ -8,5 +18,30 @@ import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class StoreProductsComponent {
-  
+  readonly activatedRouteParam$: Observable<Params> = this._activatedRoute.params
+
+  readonly categories$: Observable<CategoryModel[]> = this._categoryService.getAll()
+  readonly oneStore$: Observable<StoreModel> = this.activatedRouteParam$.pipe(
+    switchMap(data => this._storeService.getOne(data['storeId'])))
+   
+  readonly form: FormGroup = new FormGroup({
+    search: new FormControl('')
+  });
+
+  readonly productsInStore$: Observable<ProductModel[]> = combineLatest([
+    this._productService.getAll(),
+    this.activatedRouteParam$,
+    this.form.valueChanges.pipe(startWith({search: ''}))
+  ]).pipe(
+    map(([products, params, formValue]) => {
+      return products
+      .filter(product => product.storeIds.find(store => store === params['storeId']))
+      .filter(product => product.name.toLowerCase().includes(formValue.search.toLowerCase()))
+    })
+  )
+
+
+  constructor(private _categoryService: CategoryService, private _activatedRoute: ActivatedRoute, private _storeService: StoreService, private _productService: ProductService) {
+
+  }
 }
