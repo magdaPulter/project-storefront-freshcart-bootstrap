a929b1b74b243c5e1ba8d78a16bae7b2bacaab78
magdaPulter <magda.pulter@gmail.com>
2024-01-31 12:53:51 +0100

    filterValues to the new service extracted

---
+++ src/app/services/filterValues.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import { QueryParamsService } from './queryParams.service';
+import { Observable, map, shareReplay, tap } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class FilterValuesService {
+  constructor(private _queryParamsService: QueryParamsService) {}
+
+  filterValuesCheckbox(): Observable<{ stores: Set<string> }> {
+    return this._queryParamsService.getQueryParamsValues().pipe(
+      map((queryParams) => ({
+        stores: new Set<string>(
+          queryParams.stores === undefined ? [] : queryParams.stores.split(',')
+        ),
+      }))
+    );
+  }
+
+  refreshFilterByPrice(): Observable<{ priceFrom: string; priceTo: string }> {
+    return this._queryParamsService.getQueryParams().pipe(
+      map((queryParams) => {
+        return {
+          priceFrom: queryParams['priceFrom'],
+          priceTo: queryParams['priceTo'],
+        };
+      })
+      // tap((price: { priceFrom: string; priceTo: string }) =>
+      //   this.filterByPrice.patchValue(price)
+      // ),
+      // shareReplay(1)
+    );
+  }
+}
